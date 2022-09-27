53197ca882291ff1333d53f53265c0619a390725
İsmail Bilener <44755411+ismailbilener@users.noreply.github.com>
2022-09-27 08:59:38 +0300

    Add files via upload

---
+++ OgrenciNotSistemi/src/Course.java
@@ -0,0 +1,29 @@
+public class Course {
+    String name;
+    String code;
+    String prefix;
+    int note;
+    Teacher teacher;
+    int verbalNote;
+
+    Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+
+
+    }
+
+    void addTeacher(Teacher teacher) {
+        if (teacher.branch.equals(prefix)) {
+            this.teacher = teacher;
+
+        } else {
+            System.out.println("Öğretmen ve Bölüm dersleri uyuşmuyor!");
+        }
+
+    }
+
+
+}
